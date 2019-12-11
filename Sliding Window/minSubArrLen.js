9cbd7d9a4f2f71dd00a002f079b18c3eb4bb0485
Wogified-Main <8029325+Wogified@users.noreply.github.com>
2019-12-10 20:29:25 -0500

    fixed minSubArrayLen
    fixed edges case for input array with no minimum subarray and still under N

--- Sliding Window/minSubArrLen.js
+++ Sliding Window/minSubArrLen.js
@@ -16,18 +16,14 @@ function minSubArrLen(arr, n) {
   }
   var tempSum = 0;
   var lenCount = Infinity;
-  var i = 0;
-  var j = 0;
   var start = 0;
   var end = 0;
   // while loop to interate through the array
-
   while (start < arr.length) {
     //   if running sum is less than input number, add to sum
     // console.log(arr[i]);
 
     if (tempSum < n) {
-      console.log(`add ${arr[end]}`);
       tempSum += arr[end];
       if (end < arr.length) {
         end++;
@@ -35,52 +31,57 @@ function minSubArrLen(arr, n) {
     } else {
       // if the running sum is greater the input number
       // subtract the oldest value
-      console.log(`minus ${arr[start]}`);
       tempSum -= arr[start];
       start++;
     }
+    // if running sum is greater than n and current subarray is smaller than
+    // previous subarray, then update the subarr len tracker
     if ((tempSum >= n) & (end - start < lenCount)) {
       lenCount = end - start;
-      console.log(`lenCount: ${lenCount}`);
     }
 
-    console.log(`[${j}], start: ${start}, end: ${end}, Sum: ${tempSum}`);
+    // console.log(`[${j}], start: ${start}, end: ${end}, Sum: ${tempSum}`);
     j++;
+    // if the entire array has been summed, yet is still less than input number
+    // break loop
+    if ((start === 0) & (end === arr.length)) {
+      break;
+    }
   }
 
   // if running sum is still less than input num after adding all elems
   // return 0
-  if (tempSum === Infinity) {
+  if (tempSum < n) {
     return 0;
   }
   return lenCount;
 }
 
 var testCases = {
-  //   "0": {
-  //     a: [2, 3, 1, 2, 4, 3],
-  //     b: 7
-  //   },
-  //   "1": {
-  //     a: [2, 1, 6, 5, 4],
-  //     b: 9
-  //   },
-  //   "2": {
-  //     a: [3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19],
-  //     b: 52
-  //   },
-  //   "3": {
-  //     a: [1, 4, 16, 22, 5, 7, 8, 9, 10],
-  //     b: 39
-  //   },
-  //   "4": {
-  //     a: [1, 4, 16, 22, 5, 7, 8, 9, 10],
-  //     b: 55
-  //   },
-  //   "5": {
-  //     a: [4, 3, 3, 8, 1, 2, 3],
-  //     b: 11
-  //   },
+  "0": {
+    a: [2, 3, 1, 2, 4, 3],
+    b: 7
+  },
+  "1": {
+    a: [2, 1, 6, 5, 4],
+    b: 9
+  },
+  "2": {
+    a: [3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19],
+    b: 52
+  },
+  "3": {
+    a: [1, 4, 16, 22, 5, 7, 8, 9, 10],
+    b: 39
+  },
+  "4": {
+    a: [1, 4, 16, 22, 5, 7, 8, 9, 10],
+    b: 55
+  },
+  "5": {
+    a: [4, 3, 3, 8, 1, 2, 3],
+    b: 11
+  },
   "6": {
     a: [1, 4, 16, 22, 5, 7, 8, 9, 10],
     b: 95
