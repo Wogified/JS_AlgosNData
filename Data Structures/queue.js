ee5e930fcabb7bb7553700f2e4bc788e02f82461
Wogified <8029325+Wogified@users.noreply.github.com>
2020-01-13 22:30:01 -0500

    Create queue.js

---
+++ Data Structures/queue.js
@@ -0,0 +1,16 @@
+// Implement a queue class structure
+
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null
+        this.last = null
+        this.length = 0
+    }
+}
